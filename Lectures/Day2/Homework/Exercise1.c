7c1700dffc33cc5e0ba8604214dec40101366f95
Daniel Koklev <danikoklef@gmail.com>
2022-03-18 22:10:56 +0200

    Revert "mms14"
    This reverts commit 0c4a5be31758360b8d7f0eef4f43c8b2bcfc2d91.

--- Lectures/Day2/Homework/Exercise1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    double a, b, c;
-    scanf("%lf %lf %lf", &a, &b, &c);
-    printf("%lf\n%lf\n%lf\n", a, b, c);
-    // ./exercise1 < numbers1.txt | sort -n | tail -1
-}
--- Lectures/Day4/readnums.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num1, num2;
-    double rNum;
-    int n = scanf("%d %d %lf", &num1, &num2, &rNum); //returns the number of successfull inputs
-    printf("n = %d\n", n );
-    return 0;
-}
