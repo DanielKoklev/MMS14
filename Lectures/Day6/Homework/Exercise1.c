7af88ecf5a26a11248a0e616c100dfb1b65e4dda
Daniel Koklev <danikoklef@gmail.com>
2022-03-21 18:46:21 +0200

    homework for day 6

---
+++ Lectures/Day6/Homework/Exercise1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double squareRoot(int num){
+    double temp, sqrt;
+
+    sqrt = num / 2;
+    temp = 0.0001;
+
+    while(sqrt != temp){
+        temp = sqrt;
+
+        sqrt = (num / temp + temp) / 2;
+    }
+    return sqrt;
+}
+
+int main(){
+
+    int num = 256;
+    printf("The square root of %d is %.2lf\n", num, squareRoot(num));
+
+}
