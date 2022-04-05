54af131e5596bbc16884625e71185b0f25bdb54c
Daniel Koklev <danikoklef@gmail.com>
2022-04-05 18:13:56 +0300

    added new exercises from day 17

--- Lectures/Day17/arraysum2.c
+++ Lectures/Day17/arraysum2.c
@@ -8,25 +8,50 @@ int main(){
     int arr[] = {20, 10, 56, 45, 78, 55, 1, 34};
     int len = sizeof(arr) / sizeof(*arr);
 
+    int fd[2];
+    if(pipe(fd) == -1){
+        perror("error with pipe");
+        return EXIT_FAILURE;
+    }
+    
+
     pid_t pid = fork();
     int end, start;
     if(-1 == pid){
         perror("fork error");
         return EXIT_FAILURE;
+
     } else if(0 == pid){
+        close(fd[0]);
         start = 0;
         end = len / 2;
+        int sum = 0;
+
+        for(int i = start; i < end; i++){
+            sum += arr[i];
+        }
+        write(fd[1], &sum, sizeof(sum));
+        close(fd[1]);
     } else {
+        close(fd[1]);
         start = len / 2;
         end = len;
+        int sum = 0;
+        for(int i = start; i < end; i++){
+            sum += arr[i];
+        }
+        int sumChild;
+        read(fd[0], &sumChild, sizeof(sumChild));
+        printf("Sum of parent = %d\n", sum);
+        printf("Sum of child = %d\n", sumChild);
+        printf("Total = %d\n", sum + sumChild);
+        close(fd[0]);
     }
-    int sum = 0;
-    for(int i = start; i < end; i++){
-        sum += arr[i];
-    }
+    
+    
     if(pid > 0){
         wait(NULL);
     }
-    printf("Sum = %d\n", sum);
+    
     return EXIT_SUCCESS;
 }
